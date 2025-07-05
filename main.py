119473f3ed8bf775ff0805958d279caf3cbbd8cf
bgeurten <bart.geurten@otago.ac.nz>
2025-07-05 17:36:51 +1200

    added initialisation to main

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 # main.py
-
+from sqlalchemy.orm import Session
 from db.base import Base, engine
 from db.models.inventory import Inventory
 from db.models.user import User
 from db.models.order import Order
+from db.admin_actions import AdminActions
 
 
 def initialize_database():
     """Create tables in the SQLite database if they don't exist."""
     Base.metadata.create_all(engine)
     print("Database initialized with all tables.")
+    
+def seed_initial_inventory():
+    with Session(engine) as session:
+        AdminActions.initialize_inventory(session)
 
 
 if __name__ == "__main__":
     initialize_database()
+    seed_initial_inventory()
